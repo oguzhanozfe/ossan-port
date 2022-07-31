7fa573b302b93a052f4623390ef308b5ee417a6f
oguzhanozfe <48760055+oguzhanozfe@users.noreply.github.com>
2022-08-01 02:41:37 +0300

    about and portfolio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,6 +3,18 @@ import './about.scss';
 export default function About() {
   return (
     <div className='about' id='about'>
+      <div className='wrapper'>
+      <h1>Hello</h1>
+      <p>My name is Oguzhan. I am Software Developer located at Istanbul<br/> 
+      I have a degree in Computer Science and lot of passion for creating things.<br/>
+      From Game development to Fullstack i have experince in different areas.<br/>
+      Right now i am imroving my Unreal Engine knowledge and working on my own project as hobby.<br/>
+      I used to develop Vr game and mini games for Game Jams and myself in Unity.<br/>
+      Other then that i am worked freelance web development for some clients. <br/>
+      I am always looking for new challenges and opportunities to learn new things.<br/>
+      Currently seeking to work in a company where I can grow my skills and knowledge.<br/>
+      </p>
+      </div>
 
     </div>
   )
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,20 @@ import React from 'react';
 export default function Portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src="" alt="" />
+          <h3>Shitpost</h3>
+        </div>
+      </div>
 
     </div>
   )
