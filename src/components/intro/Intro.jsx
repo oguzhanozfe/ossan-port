17516489d5fd9a87da85afe8db277feb0c9384b4
oguzhanozfe <48760055+oguzhanozfe@users.noreply.github.com>
2022-08-01 01:01:41 +0300

    bug fix

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,21 +8,32 @@ import { useEffect, useRef } from 'react';
 export default function Intro() {
 
   const textRef = useRef();
+  const mextRef = useRef();
 
   useEffect(() => {
     init(textRef.current, {
-      showCursor: false,
+      showCursor: true,
       strings: ["Create","Build","Develop"],
+      backSpeed:  50,
+      typeSpeed:  80,
+      cursorChar: "_",
     });
   }, [])
-  
+
+  useEffect(() => {
+    init(mextRef.current, {
+      showCursor: false,
+      strings: ["Oguzhan Ozdemir","Ossan"],
+      loop: false,
+    });
+  }, [])
 
 
   return (
     <div className='intro' id='intro'>
       <div className='left'>
         <h1>Hi, </h1>
-        <h2>I'm Oguzhan Ozdemir</h2>
+        <h2>I'm <span ref={mextRef}></span></h2>
         <h3>Software Developer</h3>
         <h4>Need to help create things?</h4>
         <a href='#contact' className='flat-button'>CONTACT ME</a>
@@ -32,6 +43,7 @@ export default function Intro() {
         <h5>
           <span ref={textRef}></span>
         </h5>
+        <a href='#portfolio' className='flat-button'>My Works</a>
         </div>
       </div>
     </div>
