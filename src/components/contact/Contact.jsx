381b2e78e23a551f3bfe47e097838cb29eed4a54
oguzhanozfe <48760055+oguzhanozfe@users.noreply.github.com>
2022-08-07 18:00:54 +0300

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
         <img src={mail} alt="" />
       </div>
       <div className="right">
-        <h2>Contact.</h2>
+        <h2>Contact</h2>
 
         <form onSubmit={handleSubmit}>
           <input type="name" placeholder="Name" />
