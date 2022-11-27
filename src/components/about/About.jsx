72a2617f0ab4d36e00b3097141023853ba6514c3
Oguzhan Ozdemir <48760055+oguzhanozfe@users.noreply.github.com>
2022-11-27 06:46:21 +0300

    About Reset

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,21 +6,11 @@ export default function About() {
     <div className='about' id='about'>
       <div className='wrapper'>
       <h1>About Me</h1>
-      <p>My name is Oguzhan. I am a Software Developer based in Istanbul<br/> 
-      I have a degree in Computer Science <br/>
-      From Game development to Fullstack i have experience in different areas.<br/>
-      Right now i am imroving my Unreal Engine knowledge and working on my own project as hobby.<br/>
-      I used to develop Vr game and mini games for Game Jams and myself in Unity.<br/>
-      Other then that i am worked freelance web development for some clients. <br/>
-      I am always looking for new challenges and opportunities to learn new things.<br/>
-      Currently seeking to work in a company where I can grow my skills and knowledge.<br/>
-      You can check my&nbsp;
+      <p>You can check my&nbsp;
       <a href={pdf} target="_blank" rel="noreferrer">resume</a>
       &nbsp;for more information.
       </p>
       </div>
-      
-
     </div>
   )
 }
